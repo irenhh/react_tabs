3056b0d47c25d9353057fc866ff4ed19d67c9240
Iryna <iriskag@ukr.net>
2019-07-28 15:26:22 +0300

    Updates

---
+++ precache-manifest.82885d7c12a4b63c838903c7531f8e1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63c891321c532e739de61cb0d810500",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "3c1a0bef94ede9596508",
+    "url": "/react_tabs/static/css/main.98a9cccc.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react_tabs/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3c1a0bef94ede9596508",
+    "url": "/react_tabs/static/js/main.fa7483c5.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
